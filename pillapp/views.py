24b67ecac529723c2fb1935e5c7e1d121ac981c8
seoyeonson <eo970721@gmail.com>
2022-07-04 17:03:00 +0900

    손서연#7.04/17:02
    1. js : ajax 통신

--- pillapp/views.py
+++ pillapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import requests
 
 def main(request):
     return render(request, 'index.html')
--- static/bs/js/js.js
+++ static/bs/js/js.js
@@ -1,3 +1,4 @@
+// ================= 웹캠 ====================
 var myVideoStream = document.getElementById('myVideo')     // make it a global variable
 var myStoredInterval = 0
 
@@ -25,11 +26,13 @@ function restart(){
 
       myVideoStream.srcObject = stream;   
       myVideoStream.play();
+      $("#all_info").empty();
   }, 
                    
  function(error) {
    alert('webcam not working');
   });
+
 }
 
 function takeSnapshot() {
@@ -40,11 +43,63 @@ function takeSnapshot() {
  myCTX.drawImage(myVideoStream, 0, 0, myCanvasElement.width, myCanvasElement.height);
 }
 
-function takeAuto() {
-  takeSnapshot() // get snapshot right away then wait and repeat
-  clearInterval(myStoredInterval)
-  myStoredInterval = setInterval(function(){                                                                                         
-     takeSnapshot()
- }, document.getElementById('myInterval').value);        
-}
+// function takeAuto() {
+//   takeSnapshot() // get snapshot right away then wait and repeat
+//   clearInterval(myStoredInterval)
+//   myStoredInterval = setInterval(function(){                                                                                         
+//      takeSnapshot()
+//  }, document.getElementById('myInterval').value);        
+// }
 
+
+// ================ 분석 =================
+function ocr_start(){
+  // 분석해서 name들을 리턴한다.
+  return '종근당염산에페드린정'
+}
+// ================ 알약 info =================
+// https://apis.data.go.kr/1471057/MdcinPrductPrmisnInfoService1/getMdcinPrductItem?serviceKey=aS44rJLF1zAyKGhSpnEpW5PMzxPKl0lbazmPlGKUfvYqYWQY37xmDvJmCBjco6mtNWiwUfIbBDZygIEzFmJHHg%3D%3D&item_name=%EC%A2%85%EA%B7%BC%EB%8B%B9%EC%97%BC%EC%82%B0%EC%97%90%ED%8E%98%EB%93%9C%EB%A6%B0%EC%A0%95&entp_name=(%EC%A3%BC)%EC%A2%85%EA%B7%BC%EB%8B%B9&item_permit_date=19550117&pageNo=1&numOfRows=3&bar_code=8806433032005&item_seq=195500002&start_change_date=20151216&end_change_date=20160101&order=Y
+var api_key = "NmIs7ngFqUyBQNtecDEtowyuctJEgVvLlRqU4ki%2FrukB%2BuBNnRNn3w%2BCqhYPd6HiH28HI9hyih5KppfWIC%2FN3w%3D%3D";
+// var api_key = "NmIs7ngFqUyBQNtecDEtowyuctJEgVvLlRqU4ki/rukB+uBNnRNn3w+CqhYPd6HiH28HI9hyih5KppfWIC/N3w==";
+
+$(function(){
+  $("#ocr_start").click(function(){
+    var pillNm = ocr_start();
+    console.log(pillNm);
+    
+    // http://apis.data.go.kr/1471057/MdcinPrductPrmisnInfoService1/getMdcinPrductItem?serviceKey=NmIs7ngFqUyBQNtecDEtowyuctJEgVvLlRqU4ki%2FrukB%2BuBNnRNn3w%2BCqhYPd6HiH28HI9hyih5KppfWIC%2FN3w%3D%3D&item_name=종근당염산에페드린정
+    var req_url = "http://apis.data.go.kr/1471057/MdcinPrductPrmisnInfoService1/getMdcinPrductItem?serviceKey=" + api_key + "&item_name=" + pillNm;
+    // req_url = decodeURIComponent(req_url);
+    // console.log(req_url)
+
+		$.ajax({
+			url: req_url,
+      headers: {
+        'Access-Control-Allow-Origin': req_url,
+      },
+			type: "GET",
+      cache: false,
+			success: function(data, status){
+				if(status == "success") parseXML(data);
+			},
+      complete: function(data){
+        console.log("실행");
+      },
+      error:function(request, status, error){
+        console.log("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+      },
+		})
+
+	});
+});
+
+function parseXML(xmlDOM) {
+	$(xmlDOM).find("item").each(function(){
+    console.log("들어옴");
+		value = $(this).find("MATERIAL_NAME").text();
+    console.log(value);
+	});
+
+
+	$("#pill_info").html(value);
+}
