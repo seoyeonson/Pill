c6dd8f36f70bfeca82f7da61c86e0304912e97b3
seoyeonson <93702730+seoyeonson@users.noreply.github.com>
2022-07-06 17:00:59 +0900

    손서연#7.06/17:00
    1. ocr : pill_info 메뉴 클릭 이벤트

--- pillapp/views.py
+++ pillapp/views.py
@@ -11,16 +11,24 @@ def choice(request):
     return render(request, 'choice.html')
 
 def ocr(request, pk):
+    etc = True
     if pk == 1:
         title = '처방전 분석하기'
         content = '처방전이 잘보이도록 캡쳐해주세요.'
     if pk == 2:
         title = '알약 분석하기'
         content = '알약의 문자가 보이도록 캡쳐해주세요.'
+    if pk == 3:
+        ocr_start()
+        title = ''
+        content = ''
+        etc = False
+        
 
     context = {
         'title' : title,
-        'content' : content
+        'content' : content,
+        'etc' : etc,
     }
 
     return render(request, 'ocr.html', context)
@@ -32,11 +40,17 @@ def ocr_start(request):
     context = {}
 
     # =============== 처방전 및 알약 분석 =================
+    # http://apis.data.go.kr/1471057/MdcinPrductPrmisnInfoService1/getMdcinPrductItem?serviceKey=NmIs7ngFqUyBQNtecDEtowyuctJEgVvLlRqU4ki%2FrukB%2BuBNnRNn3w%2BCqhYPd6HiH28HI9hyih5KppfWIC%2FN3w%3D%3D&item_name=종근당염산에페드린정
+    
+    # ***** 변경 필요 *****
+    # 분석으로 가져온 알약명
     items_name = ['종근당염산에페드린정', '타이레놀정500밀리그람(아세트아미노펜)']
+    
+    # 공공데이터포털 알약 정보
     url = 'http://apis.data.go.kr/1471057/MdcinPrductPrmisnInfoService1/getMdcinPrductItem'
     api_key = f"NmIs7ngFqUyBQNtecDEtowyuctJEgVvLlRqU4ki/rukB+uBNnRNn3w+CqhYPd6HiH28HI9hyih5KppfWIC/N3w==";
-    # http://apis.data.go.kr/1471057/MdcinPrductPrmisnInfoService1/getMdcinPrductItem?serviceKey=NmIs7ngFqUyBQNtecDEtowyuctJEgVvLlRqU4ki%2FrukB%2BuBNnRNn3w%2BCqhYPd6HiH28HI9hyih5KppfWIC%2FN3w%3D%3D&item_name=종근당염산에페드린정
 
+    # 인식된 알약명만큼 정보 가져오기.
     for item_name in items_name:
         params ={'serviceKey' : api_key, 'item_name' : item_name}
 
@@ -60,8 +74,8 @@ def ocr_start(request):
     return HttpResponse(json.dumps(context), content_type="application/json")
 
 
+# xml파일에서 가져오려는 정보의 형태에 따라 바뀌는 함수
 def read_info(item, tag):
-    # print(item)
     result = []
     if tag == 'title':
         for i in item:
@@ -77,5 +91,6 @@ def read_info(item, tag):
             for t in texts:
                 info_str.append(t.text.strip())
                 last_str = '<br>'.join(info_str)
+
         result.append(last_str)
     return result
