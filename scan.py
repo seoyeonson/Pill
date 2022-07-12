da9c1d5ac460ea40b60f0a1ad62a834884d28020
vbvbvbvbbv <dnwngustmd@naver.com>
2022-07-12 16:13:53 +0900

    이현승 #07.12 16:10

---
+++ scan.py
@@ -0,0 +1,70 @@
+
+def four_point_transform(image, pts):
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype = "float32")
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    return warped
+
+
+def order_points(pts):
+	rect = np.zeros((4, 2), dtype = "float32")
+	s = pts.sum(axis = 1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+	diff = np.diff(pts, axis = 1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+	return rect
+
+import numpy as np
+import cv2
+import imutils
+
+image = cv2.imread('ch1.jpg') # 여기에 사진 경로 입력..
+ratio = image.shape[0] / 500.0
+orig = image.copy()
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(gray, 75, 200)
+
+cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
+for c in cnts:
+	peri = cv2.arcLength(c, True)
+	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+	if len(approx) == 4:
+		screenCnt = approx
+		break
+
+screenCnt.resize(4,2)
+dw = 800
+dh = round(dw * 297 / 210)  # A4 용지 크기: 210x297cm
+dstQuad = np.array([[0, 0], [0, dh-1], [dw-1, dh-1], [dw-1, 0]], np.float32)
+sum_ = screenCnt.sum(axis=1)
+diff = np.diff(screenCnt, axis=1)
+tl = screenCnt[np.argmin(sum_)]
+br = screenCnt[np.argmax(sum_)]
+tr = screenCnt[np.argmin(diff)]
+bl = screenCnt[np.argmax(diff)]
+
+pers = cv2.getPerspectiveTransform(np.array([tl, bl, br, tr], dtype=np.float32), dstQuad)
+dst = cv2.warpPerspective(image, pers, (dw, dh), flags=cv2.INTER_CUBIC)
+
+cv2.imshow('original', imutils.resize(image, height = 1080))
+cv2.imshow('dst', imutils.resize(dst, height = 1080))
+cv2.waitKey()
+cv2.destroyAllWindows()
